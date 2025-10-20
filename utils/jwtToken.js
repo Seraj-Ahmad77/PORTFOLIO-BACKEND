79e95c4bec5ff6d70e2ab6f182b31fe31e296efd
Seraj Ahmad <serajansari7704@gmail.com>
2025-10-20 15:22:35 +0530

    Fix cookie SameSite setting to Lax in jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,7 +7,7 @@ export const generateToken = (user, message, statusCode, res) => {
         Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
-      sameSite: "none", // 👈 cross-origin ke liye zaroori
+      sameSite: "lax", // 👈 cross-origin ke liye zaroori
       secure: true,
     })
     .json({
